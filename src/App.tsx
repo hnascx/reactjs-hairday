ad43c0daedd317ac3d663b8b18fa647381e20cf3
hnascx <henrique.nascimento12@hotmail.com>
2025-10-28 16:51:44 -0300

    chore: Setup shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button"
+
 export function App() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black">
+    <div className="flex flex-col items-center justify-center h-screen bg-black gap-4">
       <h1 className="text-white text-4xl">hairday</h1>
+      <Button>Click me</Button>
     </div>
   )
 }
