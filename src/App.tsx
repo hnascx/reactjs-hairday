e1ce053102d8f9602d5ac9a6147c74eda03996d3
hnascx <henrique.nascimento12@hotmail.com>
2025-10-31 13:54:29 -0300

    feat: Add app logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { Sidebar } from "./components/layout/sidebar"
 
 export function App() {
   return (
-    <div className="bg-gray-900 text-gray-100 text-base">
+    <section className="bg-gray-900 text-gray-100 text-base">
       <section className="grid grid-cols-[1.45fr_2fr] min-h-screen max-w-6xl mx-auto py-3">
         <Sidebar />
         <Schedules />
       </section>
-    </div>
+    </section>
   )
 }
--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,10 +1,16 @@
+import { ClientSection } from "@/components/ui/client-section"
 import { DatePicker } from "@/components/ui/date-picker"
 import { HourRange } from "@/components/ui/hour-range"
-import { ClientSection } from "@/components/ui/client-section"
 
 export function Sidebar() {
   return (
-    <aside className="bg-gray-700 rounded-xl px-18 py-20">
+    <aside className="relative flex flex-col items-center justify-center bg-gray-700 rounded-xl px-18 py-20">
+      <img
+        src="/src/styles/assets/hairday-logo.svg"
+        alt="Hair Day Logo"
+        className="absolute -left-3 -top-3 z-50 pointer-events-none w-auto h-[56px] max-w-[139px] select-none bg-gray-600 rounded-br-xl px-5"
+        draggable={false}
+      />
       <div className="flex flex-col items-start justify-start gap-5">
         <section className="flex flex-col gap-2">
           <h1 className="text-title-lg font-bold flex flex-row items-start justify-start">
